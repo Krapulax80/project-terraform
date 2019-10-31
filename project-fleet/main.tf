f33057dde7f8205bf1fcbff924b1332fb8bb5135
Fabrice Semti <emilfabrice@gmail.com>
2019-10-31 14:02:01 +0000

    first deployment test

--- project-fleet/main.tf
+++ project-fleet/main.tf
@@ -1,29 +1,27 @@
-# Variable definitions
-#variable "vsphere_user" {}
-#variable "vsphere_password" {}
-#variable "vsphere_server" {}
-
-# Define the number of resouces to be deployed
+################################################################################ DEFINE VARIABLES ################################################################################
+# Template to be used
 variable "source_vm" {
   default = "Windows 2019 Std 100GB"
 }
-# Define a prefix for the VM name and guest hostname
+# Name of the VM or VMs
 variable "name_prefix" {
-  default = "FTVXDC"
+  default = "SEVXSQL"
 }
-# Define the number of resouces to be deployed
+# How many - consecutive - VM-s to deploy (1 or more)
 variable "new-vms" {
-  default = 2
+  default = 1
 }
 # Use an offset to start counting from a certain number
-# or else the first server will be named server-01 and receive an ip address 192.168.105.51
+# or else the first server will be named server-01 and receive an ip address 172.16.20.x- or what ever starting value you define in the next two variable
 variable "offset" {
-  default = 1
+  default = 0
 }
 # Start number in last octect of ipv4 address
 variable "start_ipv4_address" {
   default = 190
 }
+
+#################################################################### SYSTEM PARAMETERS - no need to chagne ####################################################################
 
 # Initialize vSphere provider, variables can be assigned with the var-file terraform parameter
 provider "vsphere" {
@@ -31,7 +29,7 @@ provider "vsphere" {
   password       = "${var.vsphere_password}"
   vsphere_server = "${var.vsphere_server}"
 
-  # If you have a self-signed cert
+  # If you have a self-signed cert. accept it
   allow_unverified_ssl = true
 }
 
@@ -39,8 +37,15 @@ data "vsphere_datacenter" "dc" {
   name = "VxRail-Datacenter"
 }
 
-data "vsphere_datastore_cluster" "datastore_cluster" {
-  name          = "VxRail-Datacenter"
+## commented out, as we do not have cluster - instead we use datastores - EFS
+#data "vsphere_datastore_cluster" "datastore_cluster" {
+#  name          = "VxRail-Datacenter"
+#  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+#}
+
+#changed back to datastore -EFS
+data "vsphere_datastore" "datastore" {
+  name          = "VxRail-VSAN-Datastore-01"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
@@ -65,11 +70,17 @@ resource "vsphere_virtual_machine" "vm" {
   # except when an offset is used. If for example offset=1 the first name is server-02
   name             = "${var.name_prefix}${format("%02d", count.index + 1 + var.offset)}"
   resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
-  # Changed from example; datastore cluster instead of datastore
-  datastore_cluster_id = "${data.vsphere_datastore_cluster.datastore_cluster.id}"
 
-  num_cpus = 2
-  memory   = 2048
+  /*
+  #commented out, as we do not have cluster - instead we use datastores - EFS
+  datastore_cluster_id = "${data.vsphere_datastore_cluster.datastore_cluster.id}"
+  */
+
+  ## changed back to datastore - EFS
+  datastore_id = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 2 # default setting, can be adjusted / perhaps added to a variable latter
+  memory   = 2048 # default setting, can be adjusted / perhaps added to a variable latter
   guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
 
   scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
@@ -91,11 +102,13 @@ resource "vsphere_virtual_machine" "vm" {
 
     customize {
       windows_options {
-        # Define hostname using the resource count +1 making the first name server-01
-        # except when an offset is used. If for example offset=1 the first name is server-02
+        /*
+        Define hostname using the resource count +1 making the first name server-01
+        except when an offset is used. If for example offset=1 the first name is server-02
+        */
         computer_name = "${var.name_prefix}${format("%02d", count.index + 1 + var.offset)}"
-        workgroup    = "fleet-tutors.co.uk"
-        admin_password = "Master2010"
+        workgroup    = "supportingeducation.com" # default setting, can be adjusted / perhaps added to a variable latter
+        admin_password = "Yln0l4c0l" # default setting, can be adjusted / perhaps added to a variable latter
       }
 
       network_interface {
