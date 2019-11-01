1724ed310724adb214c9892e39d218b0c94a9f41
Fabrice Semti <emilfabrice@gmail.com>
2019-11-01 09:16:04 +0000

    spaceing removal

--- project-fleet/main.tf
+++ project-fleet/main.tf
@@ -24,12 +24,10 @@ variable "start_ipv4_address" {
 variable "vm_CPUs" {
   default = 2
 }
-
 # Amount of RAM
 variable "vm_RAM" {
   default = 8096
 }
-
 # Folder to crate the VM in
 variable "vm_folder" {
   default = "PreProduction"
@@ -37,11 +35,8 @@ variable "vm_folder" {
 # Notes on the server. Update if needed
 variable "vm_annotation" {
   default = "Created by Terraform"
-
 }
-
 ## SYSTEM PARAMETERS - no need to change ############################################################################################################################################################################################################
-
 # Initialize vSphere provider, variables can be assigned with the var-file terraform parameter
 provider "vsphere" {
   user           = "${var.vsphere_user}"
@@ -51,39 +46,32 @@ provider "vsphere" {
   # If you have a self-signed cert. accept it
   allow_unverified_ssl = true
 }
-
 data "vsphere_datacenter" "dc" {
   name = "VxRail-Datacenter"
 }
-
 ## commented out, as we do not have cluster - instead we use datastores - EFS
 #data "vsphere_datastore_cluster" "datastore_cluster" {
 #  name          = "VxRail-Datacenter"
 #  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 #}
-
 #changed back to datastore -EFS
 data "vsphere_datastore" "datastore" {
   name          = "VxRail-VSAN-Datastore-01"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
 data "vsphere_compute_cluster" "cluster" {
   name          = "VxRail-VSAN-Cluster-01"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
 data "vsphere_network" "network" {
   name          = "LAN"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
 data "vsphere_virtual_machine" "template" {
   name          = "${var.source_vm}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 ### RESOURCE CREATION ############################################################################################################################################################################################################
-
 # This is the actual creation process
 resource "vsphere_virtual_machine" "vm" {
   count = "${var.new-vms}"
@@ -91,35 +79,29 @@ resource "vsphere_virtual_machine" "vm" {
   # except when an offset is used. If for example offset=1 the first name is server-02
   name             = "${var.name_prefix}${format("%02d", count.index + 0 + var.offset)}"
   resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
-
   /*
   #commented out, as we do not have cluster - instead we use datastores - EFS
   datastore_cluster_id = "${data.vsphere_datastore_cluster.datastore_cluster.id}"
   */
-
   ## changed back to datastore - EFS
   datastore_id = "${data.vsphere_datastore.datastore.id}"
-
   annotation = "${var.vm_annotation}"
   num_cpus = "${var.vm_CPUs}"
   memory   = "${var.vm_RAM}"
   folder   =  "${var.vm_folder}"
   guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
-
   scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
 
   network_interface {
     network_id   = "${data.vsphere_network.network.id}"
     adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
   }
-
   disk {
     label            = "disk0"
     size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
     eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
     thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
   }
-
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
 
@@ -143,5 +125,4 @@ resource "vsphere_virtual_machine" "vm" {
     }
   }
 }
-
 ################################################################################################################################################################################################################################################################################
